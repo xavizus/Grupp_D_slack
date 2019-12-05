a5e4d381391eee2085fc0b1f498bbde5f967d483
Stephan Ljungros <stephan.ljungros@gmail.com>
2019-12-05 10:25:50 +0100

    Added new way to connect to db

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 // load morgan (logging)
 let logger = require('morgan');
-
 // load route for api
 let apiRouter = require('./routes/api');
-
+// load monk (Database helper)
+let monk = require('monk');
 // Initialize express
 let app = express();
+
+//load environment configs from file .env 
+require('dotenv').config({
+    path: __dirname + '/.env'
+});
+
+// Monk DB-connection
+const db = monk(`${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER}/${process.env.MONGODB_DATABASE}?authSource=${process.env.MONGODB_DATABASE}&w=1`);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,10 +31,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
 // create route for our api
 app.use('/api/v1', apiRouter);
 
-app.get('/login',(request, response)=> {
-  response.render('login',{title: "Discord V2"});
+app.get('/login', (request, response) => {
+    response.render('login', { title: "Discord V2" });
+});
+
+app.get('/newUser', (request, response) => {
+    response.render('newUser', { title: "Discord V2" });
+});
+
+
+app.post('/newUser', (request, response) => {
+
 });
 app.listen(8080);
--- routes/api.js
+++ routes/api.js
@@ -1,57 +1,23 @@
-// load MongoClient
-const MongoClient = require('mongodb').MongoClient;
-
-//load environment configs from file .env 
-require('dotenv').config({
-    path: __dirname + '/../.env'
-});
-
-// URL connection-string to mongoDB server
-const url = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_SERVER}/${process.env.MONGODB_DATABASE}?authSource=${process.env.MONGODB_DATABASE}&w=1`;
-
-// Create a new client
-const client = new MongoClient(url, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-});
-
 let express = require('express');
 let router = express.Router();
 
 router.get('/', (request, response) => {
-        // Connect to mongoDB server
-    client.connect(function (err) {
-        // Throw error if error is encountered
-        if (err) throw err;
 
-        console.log("Successfully connected to database!");
+    let db = request.db;
 
-        // store our current connection to the database
-        const db = client.db();
+    let collection = db.get('users');
 
-        // Find all users
-        let foundUsers = db.collection('users').find({});
+    collection.find({}, {}, function(e, docs) {
+        // Create a response object
+        let responseObject = {
+            response: "OK"
+        };
+        // store our result in response object
+        responseObject.result = docs;
 
-        // convert the result to an array.
-        foundUsers.toArray((err, result)=> {
-            // if error, throw that error
-            if (err) throw err;
-
-            // Create a response object
-            let responseObject = {
-                response: "OK"
-            };
-            // store our result in response object
-            responseObject.result = result;
-
-            // Send that object through the api.
-            response.send(responseObject);
-        });
-
-        // closes the database connection.
-        client.close();
+        // Send that object through the api.
+        response.send(responseObject);
     });
-    
 });
 
 module.exports = router;
