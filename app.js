9f7c3e199a7072dc168c0cf257139bc42865b41c
Stephan Ljungros <stephan.ljungros@gmail.com>
2019-12-17 10:40:58 +0100

    Alright, this is the TRUE fix, I promise!

--- app.js
+++ app.js
@@ -350,6 +350,25 @@ app.get('/chatroom/:room', async function (req, res) {
         res.redirect('/login');
     }
 
+    // Prepare post request to api.
+    let dataToSend = {
+        userId: request.session.userId,
+        status: "Online"
+    };
+
+    // update the status for the user that just logged in.
+    let receivedData = await fetch(`${apiURL}/updateStatus`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+    }).then(response => response.json());
+    // if we did not recive an OK message, respond with error message.
+    if (!receivedData.result == "OK") {
+        response.send(receivedData.message);
+    }
+
     // Get all current status.
     let allUsersStatuses = await fetch(`${apiURL}/status`)
         .then(response => response.json());
